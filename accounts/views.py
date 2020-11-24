be7b8c12852d0f22d53bec5756898b7ecfcb61ce
PrantaChakraborty <cbpranta@gmail.com>
2020-11-24 13:30:09 +0600

    implemented login successfully

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        if request.POST.get('username') and request.POST.get('password'):
+            user = authenticate(username=request.POST.get['username'],
+                                password=request.POST.get['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Account Disable')
+            else:
+                return HttpResponse('Invalid Login')
+
+    return request(request, 'accounts/login.html')
